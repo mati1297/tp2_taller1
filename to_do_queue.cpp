6818462456ac6923225ae6516f88a972f5a0df13
Matias Charrut <mcharrut@fi.uba.ar>
2021-10-18 17:13:32 -0300

    Ahora la cola bloqueante es tambien bounded, limitando el numero de tokens en espera al numero de workers

--- to_do_queue.cpp
+++ to_do_queue.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "to_do_queue.h"
 
 ToDoQueue::ToDoQueue(const uint8_t & max_tokens_): m(), cv_empty(),
@@ -5,9 +6,9 @@ ToDoQueue::ToDoQueue(const uint8_t & max_tokens_): m(), cv_empty(),
 
 void ToDoQueue::push(const ToDoToken & new_token) {
     std::unique_lock<std::mutex> u_lock(m);
+    cv_full.wait(u_lock, [&] { return queue.size() < max_tokens; });
     queue.push(new_token);
     cv_empty.notify_all();
-    // TODO hacerla bounded
 }
 
 
@@ -16,6 +17,7 @@ ToDoToken ToDoQueue::pop(){
     cv_empty.wait(u_lock, [&] { return !queue.empty(); });
     ToDoToken token = queue.front();
     queue.pop();
+    cv_full.notify_all();
     return token;
 }
 
